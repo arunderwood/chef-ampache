ca801bb5593d15b8a8ffcf1fdec9f6e27f03f2ad
Andrew Underwood <arunderwood@gmail.com>
2016-11-17 20:35:20 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-ampache
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'chef-ampache'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures chef-ampache'
+long_description 'Installs/Configures chef-ampache'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/chef-ampache/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/chef-ampache' if respond_to?(:source_url)
